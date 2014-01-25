764c1a5e0f6b81c5c792c2aa7c3e9fd79c6d656a
Kevin McGaire <kevinmcgaire@Kevin.local>
2014-01-24 20:49:01 -0500

    Main View Controller

---
+++ who-owes-who/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  who-owes-who
+//
+//  Created by Kevin McGaire on 1/24/2014.
+//  Copyright (c) 2014 Mitchell van der Hoeff. All rights reserved.
+//
+
+
+
+@interface MainViewController : UIViewController
+
+@end
