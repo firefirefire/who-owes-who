d4a7366925d490c3eb6d68de1e8a20ea248e2839
Mitchell Vanderhoeff <mitchellvanderhoeff@gmail.com>
2014-01-24 20:39:17 -0500

    First commit. Empty project.

---
+++ who-owes-who/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  who-owes-who
+//
+//  Created by Mitchell Van Der Hoeff on 1/24/2014.
+//  Copyright (c) 2014 Mitchell van der Hoeff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ who-owes-whoTests/who_owes_whoTests.m
@@ -0,0 +1,34 @@
+//
+//  who_owes_whoTests.m
+//  who-owes-whoTests
+//
+//  Created by Mitchell Van Der Hoeff on 1/24/2014.
+//  Copyright (c) 2014 Mitchell van der Hoeff. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface who_owes_whoTests : XCTestCase
+
+@end
+
+@implementation who_owes_whoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
